a5b5c951dccda8e80290165ef7d71085c4ecd64a
Kenchanapalli Balaji <kallurbalaji123@gmail.com>
2025-06-09 06:34:50 +0530

    commit

---
+++ Java8_Features1/src/Stream_API/Find_Maximum_Repeated_Number.java
@@ -0,0 +1,22 @@
+package Stream_API;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Find_Maximum_Repeated_Number 
+{
+	public static void main(String[] args) 
+	{
+		List<Integer> list=Arrays.asList(1,2,3,4,5,1,2,3,3,3);
+		Integer maximumrepeatedNumber = list.stream()
+				.collect(Collectors.groupingBy(n->n,Collectors.counting()))
+				.entrySet().stream()
+				.max(Map.Entry.comparingByValue())
+				.map(Map.Entry::getKey)
+				.orElse(null);
+		System.out.println(maximumrepeatedNumber);
+	}
+	
+}
